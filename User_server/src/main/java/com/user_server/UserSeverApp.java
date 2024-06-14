099c26b78d1c1318a7843f8448faf0b1b3f39b3b
test <3248556859@qq.com>
2024-06-14 13:59:49 +0800

    首先配置UserService的环境

---
+++ User_server/src/main/java/com/user_server/UserSeverApp.java
@@ -0,0 +1,12 @@
+package com.user_server;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSeverApp {
+    public static void main(String[] args) {
+        SpringApplication.run(UserSeverApp.class,args);
+    }
+}
---
+++ src/test/java/com/yucg/YucgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yucg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YucgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
